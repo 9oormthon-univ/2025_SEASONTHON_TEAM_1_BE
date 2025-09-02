90c2038266eb1aec94122db98f0830d6b3b3d704
luna <lundreame34@gmail.com>
2025-09-02 23:29:56 +0900

    Initialize Spring Boot project with Gradle build system

---
+++ src/main/java/com/goormthonuniv/cleannews/CleannewsApplication.java
@@ -0,0 +1,13 @@
+package com.goormthonuniv.cleannews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleannewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleannewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goormthonuniv/cleannews/CleannewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthonuniv.cleannews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleannewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
